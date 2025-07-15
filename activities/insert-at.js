2dcd3047e4d17dead03696d80a89a9ee94caa14b
Bryan Lazo <blazo.dev@gmail.com>
2025-07-15 12:17:37 -0500

    Refactor code for consistency and readability in insertAt and removeAt functions; fix HTML semantics in contact-earworm and fix-html-starter files.

--- activities/insert-at.js
+++ activities/insert-at.js
@@ -1,14 +1,14 @@
 function insertAt(arr, value, index) {
-  if(index < 0 || index >= arr.length) throw new Error("Invalid index");
-  for(let i = arr.length; i > index; i--){
-    arr[i]=arr[i - 1]
-  }
-  
-  arr[index] = value
-  
-  return arr;
+    if (index < 0 || index > arr.length) throw new Error("Invalid index");
+    for (let i = arr.length; i > index; i--) {
+    arr[i] = arr[i - 1]
+    }
+
+    arr[index] = value;
+
+    return arr;
 }
 
 // Example
-console.log(insertAt([1, 2, 4,5,6,7,8], 3, 2)); // [1, 2, 3, 4]
+console.log(insertAt([1, 2, 4, 5, 6, 7, 8], 3, 2)); // [1, 2, 3, 4]
 console.log(insertAt(["a", "c"], "b", 1)); // ["a", "b", "c"]
--- activities/remove-at.js
+++ activities/remove-at.js
@@ -1,21 +1,21 @@
 // removeAt
 
-const arr = [1,2,3,4,5,6]
+const arr = [1, 2, 3, 4, 5, 6];
 const at = 2;
 
 function removeAt(arr, at) {
-  if(at < 0 || at >= arr.length) return undefined;
-  const removedElement = arr[at]
-  
-  for(let i = at;i < arr.length - 1; i++){
-    arr[i]=arr[i+1]
-  }
-  
-  arr.length--;
-  
-  return removedElement;
+    if (at < 0 || at >= arr.length) return undefined;
+    const removedElement = arr[at];
+
+    for (let i = at; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1]
+    }
+
+    arr.length--;
+
+    return removedElement;
 }
 
-console.table({previousArr: arr})
-removeAt(arr, at)
-console.table({modifiedArr: arr})
+console.table({ previousArr: arr });
+removeAt(arr, at);
+console.table({ modifiedArr: arr });
